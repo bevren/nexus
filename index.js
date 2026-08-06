becd021f74d814e1908321e12052db2580d77a9e
Batuhan Evren <batuhanevren1@gmail.com>
2026-08-06 23:24:26 +0300

    Speed up shine sweep to 12x

--- index.js
+++ index.js
@@ -4988,7 +4988,7 @@ function updateThinkingAnimationState() {
     }
 
     thinkingFrameIndex = (thinkingFrameIndex + 1) % THINKING_FRAMES.length;
-    shineFrameIndex += 5;
+    shineFrameIndex += 12;
     markDirty();
     renderFrame(false);
   }, THINKING_ANIMATION_INTERVAL_MS);
